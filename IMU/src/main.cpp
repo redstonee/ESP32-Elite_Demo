646d733efc00a247b0ef1048f01e740fb2cf2de2
redstonee <1516235445@qq.com>
2025-05-17 09:48:39 +0800

    Add Teleplot data format support

--- IMU/src/main.cpp
+++ IMU/src/main.cpp
@@ -1,9 +1,37 @@
 #include <Arduino.h>
 #include <Adafruit_LSM6DS3.h>
+#include <MadgwickAHRS.h>
 #include "config.h"
 
+// #define USE_TELEPLOT
 
+constexpr uint8_t IMU_ADDRESS = 0x6B; // I2C address of the LSM6DS3 IMU
 Adafruit_LSM6DS3 imu;
+Madgwick filter;
+
+void turnOnLED(uint8_t ledNum)
+{
+  constexpr uint8_t ledPins[] = {LED1_PIN, LED2_PIN, LED3_PIN, LED4_PIN};
+
+  for (auto &pin : ledPins)
+  {
+    digitalWrite(pin, 0);
+  }
+  digitalWrite(ledPins[ledNum], 1);
+}
+
+void ledTask(void *param)
+{
+  uint8_t n = 0;
+  while (1)
+  {
+    // Blink the LEDs in sequence
+    turnOnLED(n++);
+    n %= 4;
+    delay(500);
+  }
+}
+
 void setup()
 {
   // put your setup code here, to run once:
@@ -12,9 +40,12 @@ void setup()
   Serial.begin(115200);
 
   pinMode(LED1_PIN, OUTPUT);
+  pinMode(LED2_PIN, OUTPUT);
+  pinMode(LED3_PIN, OUTPUT);
+  pinMode(LED4_PIN, OUTPUT);
 
   Wire.setPins(IMU_SDA_PIN, IMU_SCL_PIN);
-  if (!imu.begin_I2C())
+  if (!imu.begin_I2C(IMU_ADDRESS))
   {
     Serial.println("Failed to initialize IMU");
     // Blink the LED to indicate failure
@@ -34,33 +65,75 @@ void setup()
   imu.setGyroDataRate(LSM6DS_RATE_52_HZ);
   imu.setAccelRange(LSM6DS_ACCEL_RANGE_4_G);
   imu.setGyroRange(LSM6DS_GYRO_RANGE_2000_DPS);
+
+#ifdef USE_TELEPLOT
+  // Initialize the Madgwick filter with a sample frequency of 50 Hz
+  filter.begin(50);
+#endif
+
+  // Create a task for the LED blinking
+  xTaskCreate(ledTask, "LED Task", 1024, NULL, 1, NULL);
 }
+
+#ifdef USE_TELEPLOT
+String formatToTeleplot(const String name, const float angles[3])
+{
+  String result = ">3D|" + name + ":S:cube:R:" +
+                  String(angles[0]) + ":" +
+                  String(angles[1]) + ":" +
+                  String(angles[2]);
+  return result;
+}
+
+inline float radToDeg(float rad)
+{
+  return rad * (180.0 / M_PI);
+}
+
+#endif
+
+TickType_t lastWakeTime = 0;
 
 void loop()
 {
   sensors_event_t accel, gyro, temp;
   imu.getEvent(&accel, &gyro, &temp);
 
+#ifdef USE_TELEPLOT
+  filter.updateIMU(radToDeg(gyro.gyro.x),
+                   radToDeg(gyro.gyro.y),
+                   radToDeg(gyro.gyro.z),
+                   accel.acceleration.x,
+                   accel.acceleration.y,
+                   accel.acceleration.z);
+
+  float angles[] = {filter.getRollRadians(),
+                    filter.getPitchRadians(),
+                    filter.getYawRadians()};
+
+  auto cubeStr = formatToTeleplot("My Fucking Cube", angles);
+  Serial.println(cubeStr);
+  xTaskDelayUntil(&lastWakeTime, 20);
+#else
   // Print accelerometer in m/s^2
   Serial.print("Accel: ");
   Serial.print(accel.acceleration.x);
-  Serial.print(", ");
+  Serial.print(",\t");
   Serial.print(accel.acceleration.y);
-  Serial.print(", ");
-  Serial.println(accel.acceleration.z);
+  Serial.print(",\t");
+  Serial.print(accel.acceleration.z);
 
   // Print gyroscope in rad/s
-  Serial.print("Gyro: ");
+  Serial.print(";\tGyro: ");
   Serial.print(gyro.gyro.x);
-  Serial.print(", ");
+  Serial.print(",\t");
   Serial.print(gyro.gyro.y);
-  Serial.print(", ");
-  Serial.println(gyro.gyro.z);
+  Serial.print(",\t");
+  Serial.print(gyro.gyro.z);
 
   // Print temperature in Celsius
-  Serial.print("Temp: ");
-  Serial.print(temp.temperature);
-  Serial.println();
-
-  delay(200);
+  Serial.print(";\tTemp: ");
+  Serial.println(temp.temperature);
+  xTaskDelayUntil(&lastWakeTime, 100);
+#endif
 }
