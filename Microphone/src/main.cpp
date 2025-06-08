9e469ce8d3eec8e5a512029362f199d42fb22f31
redstonee <1516235445@qq.com>
2025-06-08 10:27:58 +0800

    Use 32-bit smaple depth for Microphone demo

--- Microphone/src/main.cpp
+++ Microphone/src/main.cpp
@@ -9,7 +9,7 @@ void setup()
 
     // Configure I2S for digital microphone
     I2S.setAllPins(DMIC_SCK_PIN, DMIC_WS_PIN, DMIC_DAT_PIN, -1, -1);
-    I2S.begin(I2S_PHILIPS_MODE, 16000, 16);
+    I2S.begin(I2S_LEFT_JUSTIFIED_MODE, 16000, 32);
 
     // Set up the LED pins
     pinMode(LED1_PIN, OUTPUT);
@@ -25,37 +25,34 @@ void setup()
  */
 void turnOnLED(int n)
 {
-    digitalWrite(LED1_PIN, n > 0);
-    digitalWrite(LED2_PIN, n > 1);
-    digitalWrite(LED3_PIN, n > 2);
-    digitalWrite(LED4_PIN, n > 3);
+    digitalWrite(LED4_PIN, n > 0);
+    digitalWrite(LED3_PIN, n > 1);
+    digitalWrite(LED2_PIN, n > 2);
+    digitalWrite(LED1_PIN, n > 3);
 }
 
-TickType_t lastWakeTime = 0;
 void loop()
 {
-    constexpr auto BUFFER_SIZE = 64;
-    int16_t buffer[BUFFER_SIZE];
+    constexpr auto N_SAMPLES = 128;
+    int32_t buffer[N_SAMPLES];
 
     // Read data from the I2S microphone
     auto bytesRead = I2S.read(buffer, sizeof(buffer));
     if (!bytesRead) // No data read
         return;
 
-    auto samplesRead = bytesRead / 8;
-
     // Calculate the mean of the samples
     int32_t mean = 0;
-    for (size_t i = 0; i < samplesRead; i++)
+    for (auto sample : buffer)
     {
-        mean += static_cast<int32_t>(buffer[i]);
+        mean += sample >> 8; // Scale down the sample value
     }
-    mean /= samplesRead;
+    mean /= N_SAMPLES;
 
     // Print the mean value to the serial monitor and you can view it in Teleplot
     Serial.print(">Mic:");
     Serial.println(mean);
 
     // Turn on LEDs based on the volume
-    turnOnLED(abs(mean) / 50);
+    turnOnLED(abs(mean) / 6000);
 }
