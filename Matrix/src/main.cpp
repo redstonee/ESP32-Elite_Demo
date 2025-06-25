eeef0c1f1cd55accb5ff0aef04db5b5d2d69dc36
redstonee <1516235445@qq.com>
2025-06-25 10:23:15 +0800

    Use hardware SPI for matrix demo

--- Matrix/src/main.cpp
+++ Matrix/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
+#include <SPI.h>
 
 constexpr auto MATRIX_LOAD_PIN = 14;
 constexpr auto MATRIX_SDA_PIN = 13;
@@ -8,14 +9,14 @@ constexpr auto MATRIX_CLK_PIN = 12;
 const auto matrixText = "Goodbye, world!";
 const auto textLength = strlen(matrixText);
 
-U8G2_MAX7219_8X8_F_4W_SW_SPI mat(U8G2_MIRROR, MATRIX_CLK_PIN, MATRIX_SDA_PIN, MATRIX_LOAD_PIN, U8X8_PIN_NONE);
+U8G2_MAX7219_8X8_F_4W_HW_SPI mat(U8G2_MIRROR, MATRIX_LOAD_PIN, U8X8_PIN_NONE);
 
 void setup()
 {
   // put your setup code here, to run once:
-  Serial.begin(115200);
 
-  mat.setDisplayRotation(U8G2_R1);
+  // Set the pins and initialize the matrix display
+  SPI.begin(MATRIX_CLK_PIN, MATRIX_SDA_PIN, MATRIX_SDA_PIN);
   mat.begin();
   mat.clearBuffer();
   mat.setFont(u8g2_font_5x8_mf);
@@ -25,8 +26,9 @@ void loop()
 {
   // put your main code here, to run repeatedly:
 
-  long negX = -(textLength * 5);
-  for (long x = 8; x > negX; x--)
+  // Let the text scroll
+  auto negX = -(textLength * 5);
+  for (auto x = 8; x > negX; x--)
   {
     mat.setCursor(x, 7);
     mat.print(matrixText);
